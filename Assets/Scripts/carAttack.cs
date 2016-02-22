6165646c0f8a49335af9042dde7a216b7a2dd938
Manuel Antonio Neto Madeira Amado <maba@student.dei.uc.pt>
2016-02-22 15:57:38 +0000

    Ataca ataca!

---
+++ Assets/Scripts/carAttack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carAttack : CarBehaviour
+{
+
+    void Update()
+    {
+        //Read sensor values
+        float leftSensor = LeftLD.getLinearOutput();
+        float rightSensor = RightLD.getLinearOutput();
+
+        //Calculate target motor values
+        m_RightWheelSpeed = leftSensor * MaxSpeed;
+        m_LeftWheelSpeed = rightSensor * MaxSpeed;
+    }
+}
